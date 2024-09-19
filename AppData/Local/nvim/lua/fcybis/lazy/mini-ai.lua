bc9a7097414a5d3cc4e17f4074414bcaa97f076c
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2024-09-19 17:00:06 +0200

    feat: add mini ai and surround plugins

---
+++ AppData/Local/nvim/lua/fcybis/lazy/mini-ai.lua
@@ -0,0 +1,7 @@
+return {
+    "echasnovski/mini.ai",
+    version = "*",
+    config = {
+        n_lines = 200,
+    }
+}
---
+++ AppData/Local/nvim/lua/fcybis/lazy/mini-surround.lua
@@ -0,0 +1,15 @@
+return {
+    "echasnovski/mini.surround",
+    version = "*",
+    config = {
+        mappngs = {
+            add = "<leader>sa",       -- Add surroundng in Normal and Visual modes
+            delete = "<leader>sd",    -- Delete surrounding
+            find = "<leader>sf",      -- Find surrounding (to the right)
+            find_left = "<leader>sF", -- Find surrounding (to the left)
+            highlight = "<leader>sh", -- Highlight surrounding
+            replace = "<leader>sr",   -- Replace surrounding
+        },
+        n_lines = 100,
+    },
+}
