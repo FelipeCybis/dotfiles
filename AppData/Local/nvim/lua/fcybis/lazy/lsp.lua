497fa0a2a1e3bc8e0083faab94c00b5cf2b59547
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2024-09-19 11:10:21 +0200

    fix: fix lsp recognizing typst files

--- AppData/Local/nvim/lua/fcybis/lazy/lsp.lua
+++ AppData/Local/nvim/lua/fcybis/lazy/lsp.lua
@@ -104,6 +104,10 @@ return {
                 end,
                 tinymist = function()
                     require("lspconfig").tinymist.setup({
+                        single_file_support = true,
+                        root_dir = function()
+                            return vim.fn.getcwd()
+                        end,
                         settings = {
                             formatterMode = "typstyle",
                         },
