f6a97be369233d30c45e8e3a32777f24948574df
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2024-10-17 13:29:01 +0200

    refactor: configure lualine properly to show activated venv

--- AppData/Local/nvim/lua/fcybis/lazy/lualine.lua
+++ AppData/Local/nvim/lua/fcybis/lazy/lualine.lua
@@ -9,12 +9,35 @@ return {
                 disabled_filetypes = { "packer", "NVimTree" },
             },
             sections = {
+                lualine_a = { 'filename', 'mode' },
+                lualine_c = {
+                    {
+                        require("noice").api.status.command.get,
+                        cond = require("noice").api.status.command.has,
+                        color = { fg = "#ff9e64" },
+                    },
+                },
                 lualine_x = {
                     {
-                        require("noice").api.statusline.mode.get,
-                        cond = require("noice").api.statusline.mode.has,
+                        require("noice").api.status.mode.get,
+                        cond = require("noice").api.status.mode.has,
                         color = { fg = "#ff9e64" },
-                    }
+                    },
+                    {
+                        require("noice").api.status.search.get,
+                        cond = require("noice").api.status.search.has,
+                        color = { fg = "#ff9e64" },
+                    },
+                    'encoding', 'fileformat', 'filetype',
+                },
+                lualine_y = {
+                    { function()
+                        local venv = os.getenv("VIRTUAL_ENV") or "NO ENV"
+                        return " " .. venv
+                    end,
+                        cond = function() return vim.bo.filetype == "python" end,
+                    },
+                    'progress',
                 },
             },
         }
