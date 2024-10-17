ee627fb3fdde71e1702756deb22b18605553eaa4
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2024-10-17 13:58:52 +0200

    feat(neovim): add plugin to activate venvs from neovim

---
+++ AppData/Local/nvim/lua/fcybis/lazy/venv-selector.lua
@@ -0,0 +1,30 @@
+return {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = {
+        "neovim/nvim-lspconfig",
+        "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
+        { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+    },
+    lazy = false,
+    branch = "regexp", -- This is the regexp branch, use this for the new version
+    config = function()
+        require("venv-selector").setup({
+            settings = {
+                search = {
+                    hatch = false,
+                    poetry = false,
+                    pyenv = false,
+                    pipenv = false,
+                    anaconda_envs = false,
+                    anaconda_base = false,
+                    miniconda_envs = false,
+                    miniconda_base = false,
+                    pipx = false,
+                }
+            }
+        })
+    end,
+    keys = {
+        { "<leader>vv", "<cmd>VenvSelect<cr>", desc = "Python [v]en[v] selector" },
+    },
+}
