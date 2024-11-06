80ba23f08c26f71a8d0dc6ad85c0cd0fe6176033
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2024-11-06 13:52:38 +0100

    feat(exports): add bat theme setup and conditional wezterm
    Added conditional setup for the wezterm terminal and bat theme in the
    PowerShell exports script. The script now checks if wezterm is
    available before setting it as the terminal. It also ensures the bat
    theme directory exists, downloads the Catppuccin Frappe theme if not
    present, and sets it as the default theme.

--- Documents/PowerShell/exports.ps1
+++ Documents/PowerShell/exports.ps1
@@ -1,2 +1,22 @@
 [System.Environment]::SetEnvironmentVariable('EDITOR','nvim')
-[System.Environment]::SetEnvironmentVariable('TERMINAL','wezterm')
+
+if (Get-Command wezterm -errorAction SilentlyContinue){
+    [System.Environment]::SetEnvironmentVariable('TERMINAL','wezterm')
+}
+
+if (Get-Command bat -errorAction SilentlyContinue)
+{
+    if (-not (Test-Path "$(bat --config-dir)/themes"))
+    {
+        mkdir "$(bat --config-dir)/themes"
+    }
+
+    # check if file 'Catpuccin Frappe.tmTheme' is present 
+    if (-not (Test-Path "$(bat --config-dir)/themes/Catppuccin Frappe.tmTheme"))
+    {
+        # download the theme file
+        Invoke-WebRequest -Uri https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Frappe.tmTheme -OutFile ( New-Item -Path "$(bat --config-dir)/themes/Catppuccin Frappe.tmTheme" -Force )
+        bat cache --build
+    }
+}
+[System.Environment]::SetEnvironmentVariable('BAT_THEME','Catppuccin Frappe')
