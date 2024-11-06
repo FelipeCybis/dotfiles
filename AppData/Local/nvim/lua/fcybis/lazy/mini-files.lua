4531a4a9379bc3c5742fa348e2c7dc78ec05b447
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2024-11-06 11:05:20 +0100

    refactor(nvim-minifiles): clean default config and set command to set cwd

--- AppData/Local/nvim/lua/fcybis/lazy/mini-files.lua
+++ AppData/Local/nvim/lua/fcybis/lazy/mini-files.lua
@@ -1,55 +1,25 @@
 return {
     'echasnovski/mini.files',
     version = '*',
-    opts = {
-        -- Customization of shown content
-        content = {
-            -- Predicate for which file system entries to show
-            filter = nil,
-            -- What prefix to show to the left of file system entry
-            prefix = nil,
-            -- In which order to show file system entries
-            sort = nil,
-        },
+    config = function()
+        local MiniFiles = require('mini.files')
+        MiniFiles.setup()
 
-        -- Module mappings created only inside explorer.
-        -- Use `''` (empty string) to not create one.
-        mappings = {
-            close       = 'q',
-            go_in       = 'l',
-            go_in_plus  = 'L',
-            go_out      = 'h',
-            go_out_plus = 'H',
-            reset       = '<BS>',
-            reveal_cwd  = '@',
-            show_help   = 'g?',
-            synchronize = '=',
-            trim_left   = '<',
-            trim_right  = '>',
-        },
+        local files_set_cwd = function(path)
+            -- Works only if cursor is on the valid file system entry
+            local cur_entry_path = MiniFiles.get_fs_entry().path
+            local cur_directory = vim.fs.dirname(cur_entry_path)
+            vim.fn.chdir(cur_directory)
+            vim.notify('Directory changed to ' .. cur_directory)
+        end
 
-        -- General options
-        options = {
-            -- Whether to delete permanently or move into module-specific trash
-            permanent_delete = true,
-            -- Whether to use for editing directories
-            use_as_default_explorer = true,
-        },
-
-        -- Customization of explorer windows
-        windows = {
-            -- Maximum number of windows to show side by side
-            max_number = math.huge,
-            -- Whether to show preview of file/directory under cursor
-            preview = true,
-            -- Width of focused window
-            width_focus = 50,
-            -- Width of non-focused window
-            width_nofocus = 15,
-            -- Width of preview window
-            width_preview = 25,
-        },
-    },
+        vim.api.nvim_create_autocmd('User', {
+            pattern = 'MiniFilesBufferCreate',
+            callback = function(args)
+                vim.keymap.set('n', 'gt', files_set_cwd, { buffer = args.data.buf_id, desc = 'Set cwd to current file' })
+            end,
+        })
+    end,
     keys = {
         {
             "<leader>e",
