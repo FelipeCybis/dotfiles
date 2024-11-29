bb458c95845f7f0f809796459727be82231d45bc
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2024-11-29 16:28:40 +0100

    feat: improve wezterm statusbar

--- dot_config/wezterm/config/appearance.lua
+++ dot_config/wezterm/config/appearance.lua
@@ -1,28 +1,77 @@
 local wezterm = require('wezterm') --[[@as Wezterm]]
 
--- show the domain and workspace in the status area, for quicker and easier sanity checking
-wezterm.on('update-right-status', function(window, pane)
+wezterm.on("update-right-status", function(window, pane)
+    -- Each element holds the text for a cell in a "powerline" style << fade
+    local cells = {};
+
+    -- metadata for the pane if it is a MuxPane
     local meta = pane:get_metadata() or {}
 
-    local right_status = " domain: " .. pane:get_domain_name() .. " / workspace: " .. window:active_workspace() .. " "
-    if meta.is_tardy then
-        local secs = meta.since_last_response_ms / 1000.0
-        right_status = right_status .. string.format('tardy: %5.1fs⏳', secs)
+    -- An entry for each battery (typically 0 or 1 battery)
+    for i, b in ipairs(wezterm.battery_info()) do
+        battery_info = string.format("%.0f%%", b.state_of_charge * 100)
+        if i == 1 and meta.is_tardy then
+            local secs = meta.since_last_response_ms / 1000.0
+            battery_info = battery_info .. string.format('tardy: %5.1fs⏳', secs)
+        end
+        table.insert(cells, battery_info)
     end
-    local formated_right_statues = wezterm.format {
-        -- { Attribute = { Underline = 'Single' } },
-        { Attribute = { Intensity = "Bold" } },
-        -- { Background = { Color = '#00ff00' } },
-        { Foreground = { AnsiColor = 'Fuchsia' } },
-        { Text = right_status },
-    }
-    window:set_right_status(formated_right_statues)
+
+    -- I like my date/time in this style: "Mar 3 08:14:20"
+    local date = wezterm.strftime("%b %-d %H:%M:%S");
+    table.insert(cells, date);
+
+    -- The workspace and domain name are only shown if domain is not local
+    local domain_name = pane:get_domain_name()
+    if domain_name ~= "local" then
+        table.insert(cells, "w: " .. window:active_workspace())
+        table.insert(cells, "d: " .. domain_name)
+    end
+
+    -- The powerline < symbol
+    local LEFT_ARROW = utf8.char(0xe0b3);
+    -- The filled in variant of the < symbol
+    local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+
+    -- Color palette for the backgrounds of each cell
+    local colors = {
+        "#99ff99",
+        "#00ff00",
+        "#99ff99",
+        "#00ff00",
+    };
+
+    -- Foreground color for the text across the fade
+    local text_fg = "#1a1a1a";
+
+    -- The elements to be formatted
+    local elements = {};
+    -- How many cells have been formatted
+    local num_cells = 0;
+
+    -- Translate a cell into elements
+    function push(text, is_last)
+        local cell_no = num_cells + 1
+        table.insert(elements, { Foreground = { Color = colors[cell_no] } })
+        table.insert(elements, { Text = SOLID_LEFT_ARROW })
+        table.insert(elements, { Foreground = { Color = text_fg } })
+        table.insert(elements, { Background = { Color = colors[cell_no] } })
+        table.insert(elements, { Text = " " .. text .. " " })
+        num_cells = num_cells + 1
+    end
+
+    while #cells > 0 do
+        local cell = table.remove(cells, #cells)
+        push(cell, #cells == 0)
+    end
+
+    window:set_right_status(wezterm.format(elements))
 end)
 
 return {
     color_scheme = 'Catppuccin Frappe',
     max_fps = 100,
-    status_update_interval = 300,
+    status_update_interval = 500,
     -- tab bar
     -- hide_tab_bar_if_only_one_tab = true,
     tab_bar_at_bottom = true,
@@ -40,11 +89,11 @@ return {
     colors = {
         tab_bar = {
             active_tab = {
-                -- I use a solarized dark theme; this gives a teal background to the active tab
+                -- The green is matching my windows manager borders
                 fg_color = '#1a1a1a',
                 bg_color = '#00ff00',
                 intensity = 'Bold',
-            }
+            },
         },
         split = "#00ff00",
     },
