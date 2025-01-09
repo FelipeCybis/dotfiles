76ad95469aab650608ea57b7e7e603c1030d6a39
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2025-01-09 14:26:44 +0100

    feat(nvim): treesitter allow vim regex highlighting

--- AppData/Local/nvim/lua/fcybis/lazy/treesitter.lua
+++ AppData/Local/nvim/lua/fcybis/lazy/treesitter.lua
@@ -10,7 +10,7 @@ return {
         sync_install = false,
         auto_install = false,
         indent = { enable = true },
-        highlight = { enable = true, additional_vim_regex_highlighting = false },
+        highlight = { enable = true },
         incremental_selection = {
             enable = true,
             keymaps = {
