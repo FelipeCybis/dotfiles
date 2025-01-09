79741901e42cc6be219f703c980370506a24f746
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2025-01-09 10:26:43 +0100

    feat(nvim): override some catppuccin colors and styles for python

--- AppData/Local/nvim/lua/fcybis/lazy/catppuccin.lua
+++ AppData/Local/nvim/lua/fcybis/lazy/catppuccin.lua
@@ -6,6 +6,15 @@ return {
     config = function()
         require("catppuccin").setup({
             integrations = { harpoon = true, mason = true, which_key = true, blink_cmp = true },
+            highlight_overrides = {
+                frappe = function(frappe)
+                    return {
+                        ["@lsp.type.namespace.python"] = { fg = frappe.yellow },
+                        Type = { fg = frappe.yellow, style = { "italic" } },
+                        Function = { fg = frappe.blue, style = { "italic" } }
+                    }
+                end
+            }
         })
         vim.cmd('colorscheme catppuccin-frappe')
     end
