e7ab78459dc3ae8bb9d69b4f9d77405905a09392
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2025-01-09 10:27:04 +0100

    fix(nvim): fix treesitter to actually work

--- AppData/Local/nvim/lua/fcybis/lazy/treesitter.lua
+++ AppData/Local/nvim/lua/fcybis/lazy/treesitter.lua
@@ -10,7 +10,7 @@ return {
         sync_install = false,
         auto_install = false,
         indent = { enable = true },
-        highlight = { enable = true },
+        highlight = { enable = true, additional_vim_regex_highlighting = false },
         incremental_selection = {
             enable = true,
             keymaps = {
@@ -18,6 +18,10 @@ return {
                 node_incremental = '<c-space>',
                 node_decremental = '<c-backspace>',
             }
-        }
-    }
+        },
+    },
+
+    config = function(_, opts)
+        require("nvim-treesitter.configs").setup(opts)
+    end,
 }
