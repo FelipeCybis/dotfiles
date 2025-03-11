15d085cc4629e9d6fff1adc0594d25d07dfe855f
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2025-03-11 16:41:24 +0100

    fix(nvim): fix blink keymaps to not overlap wezterm keymaps

--- AppData/Local/nvim/lua/fcybis/lazy/lsp.lua
+++ AppData/Local/nvim/lua/fcybis/lazy/lsp.lua
@@ -177,20 +177,24 @@ return {
 
     opts = {
       keymap = {
-        ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+        preset = 'none',
+        ['<C-d>'] = { 'show_documentation', 'hide_documentation' },
         ['<C-e>'] = { 'cancel', 'fallback' },
         ['<C-y>'] = { 'accept', 'fallback' },
 
         ['<Up>'] = { 'snippet_forward', 'fallback' },
         ['<Down>'] = { 'snippet_backward', 'fallback' },
 
-        ['<Tab>'] = { 'select_next', 'fallback' },
-        ['<S-Tab>'] = { 'select_prev', 'fallback' },
         ['<C-p>'] = { 'select_prev', 'fallback' },
         ['<C-n>'] = { 'select_next', 'fallback' },
 
         ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
         ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+      },
+
+      cmdline = {
+        enabled = true,
+        completion = { menu = { auto_show = true } },
       },
 
       appearance = {
