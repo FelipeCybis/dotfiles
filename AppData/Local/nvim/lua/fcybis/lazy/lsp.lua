39d502d5b9d7fba15a528292ad357cad1fa8f1c1
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2025-03-11 16:38:59 +0100

    refactor: remove unnecessary capabilities for blink

--- AppData/Local/nvim/lua/fcybis/lazy/lsp.lua
+++ AppData/Local/nvim/lua/fcybis/lazy/lsp.lua
@@ -7,15 +7,6 @@ return {
       "j-hui/fidget.nvim",
     },
     config = function()
-      -- Add blink.cmp capabilities settings to lspconfig
-      -- This should be executed before you configure any language server
-      local lspconfig_defaults = require("lspconfig").util.default_config
-      lspconfig_defaults.capabilities = vim.tbl_deep_extend(
-        'force',
-        lspconfig_defaults.capabilities,
-        require('blink.cmp').get_lsp_capabilities()
-      )
-
       require("fidget").setup({})
       require("mason").setup({})
       require("mason-lspconfig").setup({
