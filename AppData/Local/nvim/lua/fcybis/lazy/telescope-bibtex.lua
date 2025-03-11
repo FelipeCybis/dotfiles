772bef73ffc6f6edf21ecc71d6d5060dc7cf545d
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2025-03-11 16:42:12 +0100

    refactor: comment out relescope bibtex for now

--- AppData/Local/nvim/lua/fcybis/lazy/telescope-bibtex.lua
+++ AppData/Local/nvim/lua/fcybis/lazy/telescope-bibtex.lua
@@ -1,24 +1,23 @@
 return {
-    -- "nvim-telescope/telescope-bibtex.nvim",
-    "FelipeCybis/telescope-bibtex.nvim",
-    branch = "fix-context-pandoc",
-
-    dependencies = { "nvim-telescope/telescope.nvim" },
-
-    config = function()
-        require("telescope").setup({
-            extensions = {
-                bibtex = {
-                    context = true,
-                    context_fallback = true,
-                },
-            },
-        })
-        require("telescope").load_extension("bibtex")
-    end,
-
-    keys = {
-        { mode = { "i", "n" }, "<c-b>", "<CMD>Telescope bibtex<CR>" },
-    }
-
+  -- -- "nvim-telescope/telescope-bibtex.nvim",
+  -- "FelipeCybis/telescope-bibtex.nvim",
+  -- branch = "fix-context-pandoc",
+  --
+  -- dependencies = { "nvim-telescope/telescope.nvim" },
+  --
+  -- config = function()
+  --     require("telescope").setup({
+  --         extensions = {
+  --             bibtex = {
+  --                 context = true,
+  --                 context_fallback = true,
+  --             },
+  --         },
+  --     })
+  --     require("telescope").load_extension("bibtex")
+  -- end,
+  --
+  -- keys = {
+  --     { mode = { "i", "n" }, "<c-b>", "<CMD>Telescope bibtex<CR>" },
+  -- }
 }
