b2d29e7a39e0468920c1a0fb03cf76d5df53022a
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2025-03-13 10:54:46 +0100

    refactor: change treesitter keymaps so it does not conflict with wezterm leader key

--- AppData/Local/nvim/lua/fcybis/lazy/treesitter.lua
+++ AppData/Local/nvim/lua/fcybis/lazy/treesitter.lua
@@ -1,27 +1,28 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
+  "nvim-treesitter/nvim-treesitter",
 
-    build = function()
-        require("nvim-treesitter.install").update({ with_sync = true })()
-    end,
+  build = function()
+    require("nvim-treesitter.install").update({ with_sync = true })()
+  end,
 
-    opts = {
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "python", "typescript", "rust", "markdown" },
-        sync_install = false,
-        auto_install = false,
-        indent = { enable = true },
-        highlight = { enable = true },
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = '<c-space>',
-                node_incremental = '<c-space>',
-                node_decremental = '<c-backspace>',
-            }
-        },
+  opts = {
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "python", "typescript", "rust", "markdown" },
+    sync_install = false,
+    auto_install = false,
+    indent = { enable = true },
+    highlight = { enable = true },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = 'gvn',
+        node_incremental = 'gvn',
+        scope_incremental = 'gvs',
+        node_decremental = 'gvN',
+      }
     },
+  },
 
-    config = function(_, opts)
-        require("nvim-treesitter.configs").setup(opts)
-    end,
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
