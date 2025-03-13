7cf0fc5ae49d7e340bc9a7a30b9d92e94d600a80
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2025-03-13 10:40:46 +0100

    refactor: remove harpoon c-n and c-p

--- AppData/Local/nvim/lua/fcybis/lazy/harpoon.lua
+++ AppData/Local/nvim/lua/fcybis/lazy/harpoon.lua
@@ -1,71 +1,55 @@
 return {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
-    lazy = true,
-    keys = {
-        {
-            "<leader>hl",
-            function()
-                require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
-            end,
-            desc = "[H]arpoon: quick [l]ist",
-        },
-
-        {
-            "<leader>ha",
-            function()
-                require("harpoon"):list():add()
-            end,
-            desc = "[H]arpoon: [a]dd",
-        },
-
-        {
-            "<C-h>",
-            function()
-                require("harpoon"):list():select(1)
-            end,
-            desc = "Harpoon: select buffer 1"
-        },
-
-        {
-            "<C-j>",
-            function()
-                require("harpoon"):list():select(2)
-            end,
-            desc = "Harpoon: select buffer 2"
-        },
-
-        {
-            "<C-k>",
-            function()
-                require("harpoon"):list():select(3)
-            end,
-            desc = "Harpoon: select buffer 3"
-        },
-
-        {
-            "<C-l>",
-            function()
-                require("harpoon"):list():select(4)
-            end,
-            desc = "Harpoon: select buffer 4"
-        },
-        -- Toggle previous & next buffers stored within Harpoon list
-        {
-            "<C-p>",
-            function()
-                require("harpoon"):list():prev()
-            end,
-            desc = "Harpoon: previous",
-        },
-
-        {
-            "<C-n>",
-            function()
-                require("harpoon"):list():next()
-            end,
-            desc = "Harpoon: next",
-        },
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+  lazy = true,
+  keys = {
+    {
+      "<leader>hl",
+      function()
+        require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
+      end,
+      desc = "[H]arpoon: quick [l]ist",
     },
+
+    {
+      "<leader>ha",
+      function()
+        require("harpoon"):list():add()
+      end,
+      desc = "[H]arpoon: [a]dd",
+    },
+
+    {
+      "<C-h>",
+      function()
+        require("harpoon"):list():select(1)
+      end,
+      desc = "Harpoon: select buffer 1"
+    },
+
+    {
+      "<C-j>",
+      function()
+        require("harpoon"):list():select(2)
+      end,
+      desc = "Harpoon: select buffer 2"
+    },
+
+    {
+      "<C-k>",
+      function()
+        require("harpoon"):list():select(3)
+      end,
+      desc = "Harpoon: select buffer 3"
+    },
+
+    {
+      "<C-l>",
+      function()
+        require("harpoon"):list():select(4)
+      end,
+      desc = "Harpoon: select buffer 4"
+    },
+  },
 }
