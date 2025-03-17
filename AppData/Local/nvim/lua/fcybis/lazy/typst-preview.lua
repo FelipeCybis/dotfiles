f506cd059589950063f54cdac4d505a5b0ab6a9e
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2025-03-17 19:52:30 +0100

    fix: fix commit without testing :P

--- AppData/Local/nvim/lua/fcybis/lazy/typst-preview.lua
+++ AppData/Local/nvim/lua/fcybis/lazy/typst-preview.lua
@@ -10,11 +10,11 @@ return {
     local tinymist = mason_registry.get_package("tinymist")
     local tinymist_bin = tinymist:get_install_path()
     if vim.fn.has('win32') == 1 then
-      tinymist = tinymist .. "/tinymist-win32-x64.exe"
+      tinymist_bin = tinymist_bin .. "/tinymist-win32-x64.exe"
     elseif vim.loop.os_uname().sysname == "Linux" then
-      tinymist = tinymist .. "/tinymist-linux-x64"
+      tinymist_bin = tinymist_bin .. "/tinymist-linux-x64"
     else
-      tinymist = tinymist .. "/tinymist-darwin-x64"
+      tinymist_bin = tinymist_bin .. "/tinymist-darwin-x64"
     end
 
     require("typst-preview").setup({
