ff7e0a783ccac3faf5c8525ceb1a64eb6b36524f
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2025-03-23 19:13:30 +0100

    install ansible if needed

--- .chezmoiscripts/run_onchange_after_ansible.sh
+++ .chezmoiscripts/run_onchange_after_ansible.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# Simple script to check if Ansible is installed and install it if not
+echo "Checking if Ansible is installed..."
+
+if command -v ansible &> /dev/null; then
+    echo "Ansible is already installed."
+else
+    sudo apt install -y ansible
+fi
+
 ansible-playbook $HOME/.config/ansible/bootstrap.yaml -K
 
