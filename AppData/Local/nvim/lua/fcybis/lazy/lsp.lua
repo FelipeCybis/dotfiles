17645a185c98db476b3ed86996031da52fd801fc
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2025-03-23 11:11:21 +0100

    feat(nvim): add pylsp mypy to fix mypy issues

--- AppData/Local/nvim/lua/fcybis/lazy/lsp.lua
+++ AppData/Local/nvim/lua/fcybis/lazy/lsp.lua
@@ -56,6 +56,28 @@ return {
               end
             })
           end,
+          pylsp = function()
+            require("lspconfig").pylsp.setup({
+              settings = {
+                pylsp = {
+                  plugins = {
+                    pylsp_mypy = { enabled = true, live_mode = true },
+                    autopep8 = { enabled = false },
+                    mccabe = { enabled = false },
+                    pycodestyle = { enabled = false },
+                    pyflakes = { enabled = false },
+                    jedi_completion = { enabled = false },
+                    jedi_definition = { enabled = false },
+                    jedi_hover = { enabled = false },
+                    jedi_references = { enabled = false },
+                    jedi_signature_help = { enabled = false },
+                    jedi_symbols = { enabled = false },
+                    yapf = { enabled = false },
+                  }
+                }
+              }
+            })
+          end,
           basedpyright = function()
             require("lspconfig").basedpyright.setup({
               settings = {
