5338aa924d7f9b58caf962bda8ede8cdd0aa075f
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2025-03-25 11:48:43 +0100

    fix(wezterm): try standardizing wsl domains

--- dot_config/wezterm/config/domains.lua
+++ dot_config/wezterm/config/domains.lua
@@ -1,3 +1,16 @@
+local wezterm = require('wezterm') --[[@as Wezterm]]
+
+local default_wsl_domains = wezterm.default_wsl_domains()
+local wsl_domains = {}
+for idx, dom in ipairs(default_wsl_domains) do
+  if string.match(dom.name, "^WSL:Ubuntu") then
+    dom.default_cwd = "/home/fpereir"
+    dom.username = "fpereir"
+    table.insert(wsl_domains, dom)
+  end
+end
+
+
 return {
   -- ref: https://wezfurlong.org/wezterm/config/lua/SshDomain.html
   ssh_domains = {
@@ -24,12 +37,5 @@ return {
   },
 
   -- ref: https://wezfurlong.org/wezterm/config/lua/WslDomain.html
-  wsl_domains = {
-    {
-      name = 'WSL:Ubuntu',
-      distribution = 'Ubuntu',
-      username = 'fpereir',
-      default_cwd = '/home/fpereir',
-    },
-  },
+  wsl_domains = wsl_domains,
 }
