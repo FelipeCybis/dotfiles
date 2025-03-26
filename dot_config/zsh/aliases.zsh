a4af78a75f440959c1ecd990cdfd9b26155c0230
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2025-03-26 17:09:18 +0100

    add git alias to change remote auth

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -7,3 +7,7 @@ alias lla='ls -la'
 alias lat='la --tree'
 alias llt='ll --tree'
 alias llat='lla --tree'
+
+# Oneliners to switch remote from ssh to https and back
+alias git-https="git remote set-url origin https://github.com/$(git remote get-url origin | sed 's/https:\/\/github.com\///' | sed 's/git@github.com://')"
+alias git-ssh="  git remote set-url origin git@github.com:$(git remote get-url origin | sed 's/https:\/\/github.com\///' | sed 's/git@github.com://')"
