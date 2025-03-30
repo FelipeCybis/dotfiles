c7c1dec6662f71908d482c5a5d1d34a04677e637
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2025-03-30 19:21:44 +0200

    remove unused set of clipboard

--- AppData/Local/nvim/lua/fcybis/set.lua
+++ AppData/Local/nvim/lua/fcybis/set.lua
@@ -65,25 +65,4 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 -- Show which line your cursor is on
 vim.opt.cursorline = true
 
-if os.getenv("SSH_CLIENT") ~= nil or os.getenv("SSH_TTY") ~= nil then
-  local function my_paste(_)
-    return function(_)
-      local content = vim.fn.getreg('"')
-      return vim.split(content, '\n')
-    end
-  end
-
-  vim.g.clipboard = {
-    name = "OSC 52",
-    copy = {
-      ["+"] = require("vim.ui.clipboard.osc52").copy "+",
-      ["*"] = require("vim.ui.clipboard.osc52").copy "*",
-    },
-    paste = {
-      ["+"] = my_paste "+",
-      ["*"] = my_paste "*",
-    },
-  }
-end
-
 vim.lsp.set_log_level("off")
