181b6b270a8295e4db2d56194470a9905909757a
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2025-09-16 11:29:31 +0200

    feat: update venv selector to main branch

--- AppData/Local/nvim/lua/fcybis/lazy/venv-selector.lua
+++ AppData/Local/nvim/lua/fcybis/lazy/venv-selector.lua
@@ -1,12 +1,11 @@
 return {
   "linux-cultist/venv-selector.nvim",
   dependencies = {
-    "neovim/nvim-lspconfig",
     { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
   },
   lazy = false,
   event = "VeryLazy",
-  branch = "regexp", -- This is the regexp branch, use this for the new version
+  branch = "main",
   opts = {
     settings = {
       search = {
