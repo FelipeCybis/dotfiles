d3cc3330b720b64990790bdd9824621ac9871025
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2025-10-23 12:14:16 +0200

    [nvim] improve moving logic to move across nvim tabs as well

--- AppData/Local/nvim/lua/fcybis/lazy/wezterm.lua
+++ AppData/Local/nvim/lua/fcybis/lazy/wezterm.lua
@@ -15,11 +15,34 @@ return {
       vim.cmd('wincmd ' .. direction)
       local new_nvim_win = vim.api.nvim_get_current_win()
 
-      -- if nvim window did not change, try wezterm pane
+      -- if nvim window did not change, check for tabs before trying wezterm
       if current_nvim_win == new_nvim_win then
-        -- wezterm nvim plugin does not handles things well when not on local (ssh, etc))
-        -- we handle things on wezterm config
-        wezterm.set_user_var("WEZTERM_MOVE", direction)
+        local current_tab = vim.api.nvim_get_current_tabpage()
+        local list_of_tabpages = vim.api.nvim_list_tabpages()
+
+        -- tabpages are numbered but may have changed position, find current position
+        local current_tab_position
+        for i, tabpage in ipairs(list_of_tabpages) do
+          if tabpage == current_tab then
+            current_tab_position = i
+          end
+        end
+
+        local tab_changed = false
+        if direction == 'h' or direction == 'l' then
+          local next_tab = direction == 'h' and current_tab_position - 1 or current_tab_position + 1
+          if list_of_tabpages[next_tab] ~= nil then
+            vim.api.nvim_set_current_tabpage(next_tab)
+            tab_changed = true
+          end
+        end
+
+        -- if tab did not change, try wezterm pane
+        if not tab_changed then
+          -- wezterm nvim plugin does not handles things well when not on local (ssh, etc))
+          -- we handle things on wezterm config
+          wezterm.set_user_var("WEZTERM_MOVE", direction)
+        end
       end
     end
 
