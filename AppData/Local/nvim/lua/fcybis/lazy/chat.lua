ce4ca79f08844ff318d4919b97c4c42003645f89
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2025-12-01 10:50:35 +0100

    nvim: disable copilotchat

--- AppData/Local/nvim/lua/fcybis/lazy/chat.lua
+++ AppData/Local/nvim/lua/fcybis/lazy/chat.lua
@@ -1,14 +1,13 @@
 return {
-    {
-        "CopilotC-Nvim/CopilotChat.nvim",
-        branch = "main",
-        event = "VeryLazy",
-        dependencies = {
-            { "github/copilot.vim" },
-            { "nvim-lua/plenary.nvim" },
-        },
-        opts = {
-            debug = true,
-        },
-    },
+  "CopilotC-Nvim/CopilotChat.nvim",
+  branch = "main",
+  enabled = false,
+  event = "VeryLazy",
+  dependencies = {
+    { "github/copilot.vim" },
+    { "nvim-lua/plenary.nvim" },
+  },
+  opts = {
+    debug = true,
+  },
 }
