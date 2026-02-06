eb8a178d1335a84d2fdf1b74926d196067fdf5aa
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2026-02-06 16:40:00 +0100

    fix(nvim): remove space from lua command

--- AppData/Local/nvim/lua/fcybis/lazy/snacks_dashboard.lua
+++ AppData/Local/nvim/lua/fcybis/lazy/snacks_dashboard.lua
@@ -8,8 +8,8 @@ return {
         keys = {
           { icon = ' ', key = 'f', desc = 'Find File', action = ':lua Snacks.dashboard.pick("files")' },
           { icon = ' ', key = 'n', desc = 'New File', action = ':ene | startinsert' },
-          { icon = ' ', key = 'g', desc = 'Find Text', action = ':lua Snacks.dashboard.pick("live_grep ")' },
-          { icon = ' ', key = 'r', desc = 'Recent Files', action = ':lua Snacks.dashboard.pick("oldfiles ")' },
+          { icon = ' ', key = 'g', desc = 'Find Text', action = ':lua Snacks.dashboard.pick("live_grep")' },
+          { icon = ' ', key = 'r', desc = 'Recent Files', action = ':lua Snacks.dashboard.pick("oldfiles")' },
           { icon = ' ', key = 'c', desc = 'Dotfiles', action = ':lua Fzf_Chezmoi()' },
           { icon = ' ', key = 's', desc = 'Restore Session', section = 'session' },
           { icon = '󰒲 ', key = 'l', desc = 'Lazy', action = ':Lazy', enabled = package.loaded.lazy ~= nil },
