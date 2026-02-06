8d65e06abb0aedb16896a4b7887f561751a2f843
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2026-02-06 16:40:29 +0100

    refactor(nvim): disable zen-mode plugin

--- AppData/Local/nvim/lua/fcybis/lazy/zen-mode.lua
+++ AppData/Local/nvim/lua/fcybis/lazy/zen-mode.lua
@@ -1,24 +1,25 @@
 return {
-    "folke/zen-mode.nvim",
-    dependencies = { "willothy/wezterm.nvim" },
-    keys = {
-        { "<C-f>", "<CMD>ZenMode<CR>", desc = "Zen Mode" },
+  "folke/zen-mode.nvim",
+  dependencies = { "willothy/wezterm.nvim" },
+  enabled = false,
+  keys = {
+    { "<C-f>", "<CMD>ZenMode<CR>", desc = "Zen Mode" },
+  },
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    plugins = {
+      wezterm = {
+        enabled = true,
+        font = "+4",
+      },
     },
-    opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-        plugins = {
-            wezterm = {
-                enabled = true,
-                font = "+4",
-            },
-        },
-        on_open = function(win)
-            require("wezterm").zoom_pane()
-        end,
-        on_close = function()
-            require("wezterm").zoom_pane()
-        end,
-    }
+    on_open = function(win)
+      require("wezterm").zoom_pane()
+    end,
+    on_close = function()
+      require("wezterm").zoom_pane()
+    end,
+  }
 }
